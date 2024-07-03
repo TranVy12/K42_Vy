1a00c3903d24f7b8b5c2a7f049e99d23246c6dd3
TranVy12 <tv0968716206@gmail.com>
2024-07-03 18:56:15 +0700

    bai tap

---
+++ JS/demo.js
@@ -0,0 +1,15 @@
+function showMess1 () {
+    alert('Xin chào!')
+}
+
+function showMess2() {
+    document.getElementById('messDiv').innerHTML="Xin chào!"
+}
+
+function showMess3() {
+    document.write('Xin chào!')
+}
+
+function showMess4() {
+    console.log('Xin chào!')
+}
